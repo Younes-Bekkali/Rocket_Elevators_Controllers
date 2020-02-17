fd429ac1ac73e52ff227e2b24c5db5bdee8ea4c5
Younes Bekkali <y.bekkali@gmail.com>
2020-02-17 12:09:20 -0500

    "Odysee Week 2 --START--"

---
+++ Residential_Controller.js
@@ -0,0 +1,22 @@
+console.log("Test");
+console.log("Test2");
+
+class Column {
+    constructor() {
+    this.nbFloors = nbFloors;
+    this.nbElevators = nbElevators;
+    }
+    MoveUp(){};
+
+    MoveDown(){};
+
+    RequestElevator (RequestedFloor, Direction){} //Method 1 :  must return the chosen elevator and move the elevators in its treatment.
+
+    RequestFloor (Elevator, RequestedFloor){} // Method 2 : must move the elevators in its treatment.
+
+    
+}
+console.log("The floor where the person is");
+console.log("the direction in which he wants to go (Up or Down)");
+console.log("The elevator used");
+console.log("The requested floor");
