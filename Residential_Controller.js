048290749a7af46b9ab40dcd472a0852af93cafb
Younes Bekkali <y.bekkali@gmail.com>
2020-02-17 12:42:55 -0500

    just another modifcation

--- Residential_Controller.js
+++ Residential_Controller.js
@@ -16,7 +16,7 @@ class Column {
 
     
 }
-console.log("The floor where the person is");
+console.log("The floor where the person is: ");
 console.log("the direction in which he wants to go (Up or Down)");
-console.log("The elevator used");
-console.log("The requested floor");
+console.log("The elevator used is: ");
+console.log("The requested floor is: ");
